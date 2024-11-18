4b34dce59978bb305c44e080861606aae33b13cb
arivano81 <arivano81@gmail.com>
2024-11-18 20:13:56 +0300

    Доработано решение второго модуля, задание 4

--- module_2_4.py
+++ module_2_4.py
@@ -1,6 +1,6 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 not_primes = []
-primes= []
+primes = []
 for number in numbers:
     is_prime = True
     for i in range(2, number):
@@ -8,8 +8,8 @@ for number in numbers:
             is_prime = False
             not_primes.append(number)
             break
-    if is_prime and number!=1:
+    if is_prime and number != 1:
         primes.append(number)
-print(numbers)
+# print(numbers)
 print('Primes: ', primes)
 print('Not primes: ', not_primes)
