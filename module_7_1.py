645d4ba7de537ed2517f62b34dc1945d2d749021
arivano81 <arivano81@gmail.com>
2024-12-10 19:34:44 +0300

    Задача "Developer - не только разработчик" + PEP8

--- module_7_1.py
+++ module_7_1.py
@@ -2,12 +2,14 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors:
             print("Такого этажа не существует")
         else:
-            for i in range(1,new_floor+1):
+            for i in range(1, new_floor + 1):
                 print(f"{i}")
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
